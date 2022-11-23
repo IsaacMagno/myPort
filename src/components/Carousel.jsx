e30a764779aaba83233dea17ea67b3e1f9f8b036
IsaacMagno <isaacmagno98@gmail.com>
2022-11-23 14:50:10 -0300

    fix select on next

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -45,7 +45,7 @@ const Carousel = ({ images }) => {
         </div>
       </div>
       <button
-        className='carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0'
+        className='carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0 noSelect'
         type='button'
         data-bs-target={`#carousel-${images}`}
         data-bs-slide='prev'
@@ -57,7 +57,7 @@ const Carousel = ({ images }) => {
         <span className='visually-hidden'>Previous</span>
       </button>
       <button
-        className='carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0'
+        className='carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0 noSelect'
         type='button'
         data-bs-target={`#carousel-${images}`}
         data-bs-slide='next'
