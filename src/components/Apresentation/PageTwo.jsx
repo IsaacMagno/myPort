2178987a65f9f4e09b6c0061305657e7b8ba3a74
IsaacMagno <isaacmagno98@gmail.com>
2022-11-26 15:10:18 -0300

    correçao texto

--- src/components/Apresentation/PageTwo.jsx
+++ src/components/Apresentation/PageTwo.jsx
@@ -5,7 +5,7 @@ const PageTwo = () => {
     <div className='bg-gradient-to-r from-red-300 to-blue-300 min-h-screen flex items-center justify-center'>
       <div>
         <p className='text-xl md:text-3xl 2xl:text-6xl font-bold text-gray-800'>
-          Sou Programador a 2 anos
+          Sou Programador há 2 anos
         </p>
         <p className='text-xs md:text-base 2xl:text-3xl font-thin text-gray-800 2xl:mt-1'>
           [ NodeJS, Python, JAVA, MySQL
